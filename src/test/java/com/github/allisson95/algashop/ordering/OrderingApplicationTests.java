b7dea3fbdc75da25d583bae7c15ba33321330238
Allisson Alves Batista Nunes <allissonabn@gmail.com>
2025-11-12 23:22:17 -0300

    Initialize ordering microservice with Spring Boot and Gradle setup

---
+++ src/test/java/com/github/allisson95/algashop/ordering/OrderingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.allisson95.algashop.ordering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
