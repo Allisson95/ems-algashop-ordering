b3428baba4bf2b565f039cf81c465307f42e21b7
Allisson Alves Batista Nunes <allissonabn@gmail.com>
2025-11-18 21:27:54 -0300

    Use DataFaker for randomized test data generation in `OrderItemTest`
    Replaced hardcoded test data in `OrderItemTest` with dynamic values generated by the DataFaker library. Updated `build.gradle` to include DataFaker as a test dependency.

--- src/test/java/com/github/allisson95/algashop/ordering/domain/entity/OrderItemTest.java
+++ src/test/java/com/github/allisson95/algashop/ordering/domain/entity/OrderItemTest.java
@@ -5,22 +5,23 @@ import com.github.allisson95.algashop.ordering.domain.valueobject.ProductName;
 import com.github.allisson95.algashop.ordering.domain.valueobject.Quantity;
 import com.github.allisson95.algashop.ordering.domain.valueobject.id.OrderId;
 import com.github.allisson95.algashop.ordering.domain.valueobject.id.ProductId;
+import net.datafaker.Faker;
 import org.junit.jupiter.api.Test;
-
-import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class OrderItemTest {
+
+    Faker faker = new Faker();
 
     @Test
     void shouldGenerateOrderItem() {
         final OrderItem orderItem = OrderItem.newOrderItem()
                 .orderId(new OrderId())
                 .productId(new ProductId())
-                .productName(new ProductName("Smartphone"))
-                .price(new Money(new BigDecimal("1499.99")))
-                .quantity(new Quantity(1))
+                .productName(new ProductName(faker.commerce().productName()))
+                .price(new Money(faker.commerce().price()))
+                .quantity(new Quantity(faker.number().numberBetween(1, 10)))
                 .build();
         assertThat(orderItem).isNotNull();
     }
