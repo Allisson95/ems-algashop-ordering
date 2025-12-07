dc862ffa6911fbb8822e2ca44ea8b48b2b2e5d5b
Allisson Alves Batista Nunes <allissonabn@gmail.com>
2025-12-07 14:26:35 -0300

    Add billing and shipping assemblers to `OrderPersistenceEntityAssembler`
    Introduce helper methods to assemble `BillingEmbeddable`, `ShippingEmbeddable`, `RecipientEmbeddable`, and `AddressEmbeddable`. Enhance structured transformation for billing and shipping details in `OrderPersistenceEntity`. Fix null checks for order ID initialization.

--- src/main/java/com/github/allisson95/algashop/ordering/infrastructure/persistence/assembler/OrderPersistenceEntityAssembler.java
+++ src/main/java/com/github/allisson95/algashop/ordering/infrastructure/persistence/assembler/OrderPersistenceEntityAssembler.java
@@ -1,9 +1,17 @@
 package com.github.allisson95.algashop.ordering.infrastructure.persistence.assembler;
 
 import com.github.allisson95.algashop.ordering.domain.model.entity.Order;
+import com.github.allisson95.algashop.ordering.domain.model.valueobject.Address;
+import com.github.allisson95.algashop.ordering.domain.model.valueobject.Billing;
+import com.github.allisson95.algashop.ordering.domain.model.valueobject.Shipping;
+import com.github.allisson95.algashop.ordering.infrastructure.persistence.embeddable.AddressEmbeddable;
+import com.github.allisson95.algashop.ordering.infrastructure.persistence.embeddable.BillingEmbeddable;
+import com.github.allisson95.algashop.ordering.infrastructure.persistence.embeddable.RecipientEmbeddable;
+import com.github.allisson95.algashop.ordering.infrastructure.persistence.embeddable.ShippingEmbeddable;
 import com.github.allisson95.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
 import org.springframework.stereotype.Component;
 
+import static java.util.Objects.isNull;
 import static java.util.Objects.requireNonNull;
 
 @Component
@@ -17,9 +25,9 @@ public class OrderPersistenceEntityAssembler {
         requireNonNull(orderPersistenceEntity, "orderPersistenceEntity cannot be null");
         requireNonNull(order, "order cannot be null");
 
-//        if (isNull(orderPersistenceEntity.getId())) {
-        orderPersistenceEntity.setId(order.id().value().toLong());
-//        }
+        if (isNull(orderPersistenceEntity.getId())) {
+            orderPersistenceEntity.setId(order.id().value().toLong());
+        }
 
         orderPersistenceEntity.setCustomerId(order.customerId().value());
         orderPersistenceEntity.setTotalAmount(order.totalAmount().value());
@@ -28,13 +36,62 @@ public class OrderPersistenceEntityAssembler {
         orderPersistenceEntity.setPaidAt(order.paidAt());
         orderPersistenceEntity.setCancelledAt(order.cancelledAt());
         orderPersistenceEntity.setReadyAt(order.readyAt());
-//        orderPersistenceEntity.setBilling;
-//        orderPersistenceEntity.setShipping;
+        orderPersistenceEntity.setBilling(assembleBilling(order.billing()));
+        orderPersistenceEntity.setShipping(assembleShipping(order.shipping()));
         orderPersistenceEntity.setStatus(order.status().name());
         orderPersistenceEntity.setPaymentMethod(order.paymentMethod().name());
         orderPersistenceEntity.setVersion(order.version());
 
         return orderPersistenceEntity;
+    }
+
+    private BillingEmbeddable assembleBilling(final Billing billing) {
+        if (isNull(billing)) {
+            return null;
+        }
+
+        return BillingEmbeddable.builder()
+                .firstName(billing.fullName().firstName())
+                .lastName(billing.fullName().lastName())
+                .document(billing.document().value())
+                .phone(billing.phone().value())
+                .email(billing.email().value())
+                .address(assembleAddress(billing.address()))
+                .build();
+    }
+
+    private ShippingEmbeddable assembleShipping(final Shipping shipping) {
+        if (isNull(shipping)) {
+            return null;
+        }
+
+        return ShippingEmbeddable.builder()
+                .recipient(assembleRecipient(shipping))
+                .address(assembleAddress(shipping.address()))
+                .cost(shipping.cost().value())
+                .expectedDeliveryDate(shipping.expectedDeliveryDate())
+                .build();
+    }
+
+    private RecipientEmbeddable assembleRecipient(final Shipping shipping) {
+        return RecipientEmbeddable.builder()
+                .firstName(shipping.recipient().fullName().firstName())
+                .lastName(shipping.recipient().fullName().lastName())
+                .document(shipping.recipient().document().value())
+                .phone(shipping.recipient().phone().value())
+                .build();
+    }
+
+    private AddressEmbeddable assembleAddress(final Address address) {
+        return AddressEmbeddable.builder()
+                .street(address.street())
+                .number(address.number())
+                .complement(address.complement())
+                .neighborhood(address.neighborhood())
+                .city(address.city())
+                .state(address.state())
+                .zipCode(address.zipCode().value())
+                .build();
     }
 
 }
